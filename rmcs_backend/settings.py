10e44eb095b3e5de78db0510de41462ea535fcac
Chris Paul <79316343+package-coder@users.noreply.github.com>
2022-02-24 00:49:29 +0800

    Venv Setup

--- rmcs_backend/settings.py
+++ rmcs_backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#b1tnm#jp1zqgg_32ih&v-mevn-%7#pb4q$z_^$nsc%%pd(m8^'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-#b1tnm#jp1zqgg_32ih&v-mevn-%7#pb4q$z_^$nsc%%pd(m8^')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get('DEBUG')) == "1"
 
 ALLOWED_HOSTS = []
 
